520df8c6f3183d51df2659d5ea8ff183581fd510
konaraddio <omkar.konaraddi@gmail.com>
2017-07-22 23:53:43 -0400

    removed navbar and footer partials; updated footer across all pages

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 #For building the resources page
-mustache -p mustaches/navbar.mustache \
--p mustaches/footer.mustache \
--p mustaches/resource_card.mustache \
+mustache -p mustaches/resource_card.mustache \
 data/resources.json mustaches/resources_page.mustache > html/resources_page.html
