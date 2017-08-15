778d3d54a1214af4422e8e7d3d06f8f012e648e9
Omkar Konaraddi <omkar.konaraddi@gmail.com>
2017-08-15 19:08:51 -0400

    added minify and mustache modules and old navbar

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 sass styles.scss styles.css --sourcemap=none
 
 # Minify css
-minify styles.css > styles.min.css
+./node_modules/.bin/minify styles.css > styles.min.css
 
 # Minify js
-minify js/resources_page.js > js/resources_page.min.js
+./node_modules/.bin/minify js/resources_page.js > js/resources_page.min.js
 
 # For building the resources page
-#mustache -p mustaches/resource_card.mustache \
-#data/resources.json mustaches/resources_cards_list.mustache > html/resources_cards_list.html
+./node_modules/.bin/mustache -p mustaches/resource_card.mustache \
+data/resources.json mustaches/resources_cards_list.mustache > html/resources_cards_list.html
