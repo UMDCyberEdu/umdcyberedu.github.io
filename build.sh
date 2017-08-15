8c3205ac40415d04a42753691cccec54c270a76c
Omkar Konaraddi <omkar.konaraddi@gmail.com>
2017-08-15 18:26:40 -0400

    new, moved navbar for landing page

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 sass styles.scss styles.css --sourcemap=none
 
 # Minify css
-minify styles.css
+minify styles.css > styles.min.css
 
 # Minify js
-minify js/resources_page.js
+minify js/resources_page.js > js/resources_page.min.js
 
 # For building the resources page
-mustache -p mustaches/resource_card.mustache \
-data/resources.json mustaches/resources_cards_list.mustache > html/resources_cards_list.html
+#mustache -p mustaches/resource_card.mustache \
+#data/resources.json mustaches/resources_cards_list.mustache > html/resources_cards_list.html
